598f2311381fd09b23bf58dd2a4abac72da311b9
Julian Hammer <julian.hammer@fau.de>
2018-06-06 17:11:50 +0200

    this commit is a deadend

--- op.py
+++ op.py
@@ -1,9 +1,96 @@
 #!/usr/bin/env python3
 import copy
 import itertools
+import collections
 
 
 # TODO use abc to force implementation of interface requirements
+
+class RegisterMapping:
+    def __init__(self, prefix='reg'):
+        self.prefix = prefix
+        self.mapping = collections.defaultdict(self._unused_name)
+        self.source_sink = []
+        self.back_edges = []
+    
+    def source(self, reg):
+        print('src ', reg)
+        if any([reg in ss for ss in self.source_sink]):
+            raise ValueError("Already registered as source or sink.")
+        self.source_sink.append((reg, None))
+    
+    def sink(self, reg):
+        print('sink', reg)
+        if any([reg in ss for ss in self.source_sink]):
+            raise ValueError("Already registered as source or sink.")
+        self.source_sink.append((None, reg))
+    
+    def _unused_name(self):
+        # Find free name
+        i = 0
+        while True:
+            name = '%{}.{}'.format(i, self.prefix)
+            if name not in self.mapping.values():
+                break
+            i += 1
+        return name
+    
+    def connect(self, regs):
+        name = self._unused_name()
+
+        has_source = None
+        sinks = []
+        sinks_before_source = []
+        for source, sink in self.source_sink:
+            if source in regs:
+                if has_source is not None:
+                    raise ValueError('Connected registers may only have one source. '
+                                     'Multiple found.')
+                has_source = source
+            elif sink in regs:
+                sinks.append(sink)
+                if has_source is not None:
+                    sinks_before_source.append(sink)
+
+        if has_source is None:
+            raise ValueError('No sources found in given register list. Connected registers need to '
+                             'have one source.')
+        if not sinks:
+            raise ValueError('No sinks found in given register list. Need at least one sink.')
+
+        for reg in regs:
+            if not any([reg in ss for ss in self.source_sink]):
+                raise ValueError("Register has not been registered as sink nor source.")
+
+        for r in regs:
+            self.mapping[r] = name
+            if r in sinks_before_source:
+                self.mapping[r] += '_'
+
+        if sinks_before_source:
+            self.back_edges.append((has_source, sinks[0]))
+        
+        return name
+
+    def connected(self, regs):
+        """Return True if all registers in regs are connected."""
+        # Compile list of names, with '_' stripped from right
+        names = [self.mapping[r].rstrip('_') for r in regs]
+        # check that all entries are equal
+        return not names or names.count(names[0]) == len(names)
+
+    def set_constant(self, reg, value):
+        """Set register to constant value."""
+        if reg in self.mapping:
+            raise ValueError("Register is already connected/set constant elsewhere.")
+        self.mapping[reg] = value
+    
+    def get_ir_repr(self, reg):
+        if reg in self.mapping:
+            return self.mapping(reg)
+        else:
+            raise ValueError("Register has not been connected.")
+
 
 class Operand:
     def __init__(self, llvm_type):
@@ -16,7 +103,7 @@ class Operand:
         raise NotImplementedError()
 
     def __repr__(self):
-        return '{}({})'.format(
+        return hex(id(self))+'{}({})'.format(
             self.__class__.__name__,
             ', '.join(['{}={!r}'.format(k,v) for k,v in self.__dict__.items()
                        if not k.startswith('_')]))
@@ -71,82 +158,29 @@ class MemoryReference(Operand):
             raise ValueError("Must provide at least an offset or base.")
         
     def get_ir_repr(self):
-        pass # TODO
+        raise NotImplementedError("TODO")
 
     def get_constraint_char(self):
         return 'm'
 
 
 class Register(Operand):
-    # Persistent storage of register names
-    _REGISTER_NAMES_IN_USE = {}
-    
-    @classmethod
-    def reset(cls):
-        cls._REGISTER_NAMES_IN_USE = {}
-    
-    @staticmethod
-    def match(source_registers, destination_registers):
-        matches = set()
-        unmatched = set()
-        for src in source_registers:
-            matched = False
-            for dst in destination_registers:
-                if src.llvm_type == dst.llvm_type:
-                    destination_registers.remove(dst)
-                    src.join(dst)
-                    matches.add(src)
-                    matched = True
-            if not matched:
-                unmatched.add(src)
-        return matches, unmatched
-    
-    def __init__(self, llvm_type, constraint_char='r', name='reg'):
+    def __init__(self, llvm_type, constraint_char='r'):
         self.llvm_type = llvm_type
         self.constraint_char = constraint_char
-        assert len(name) > 0, "name needs to be at least of length 1."
-        if name in self._REGISTER_NAMES_IN_USE:
-            i = 0
-            name_test = name
-            while name_test in self._REGISTER_NAMES_IN_USE:
-                name_test = '{}.{}'.format(name, i)
-                i += 1
-            name = name_test
-        self._name = name
-        self._REGISTER_NAMES_IN_USE[self._name] = '%"{}"'.format(self._name)
     
-    def get_ir_repr(self):
-        return self._REGISTER_NAMES_IN_USE[self._name]
-    
-    def set_to_constant(self, value):
-        self._REGISTER_NAMES_IN_USE[self._name] = value
+    def get_ir_repr(self, reg_mapping):
+        return reg_mapping.get_ir_repr(self)
     
     def get_constraint_char(self):
         return self.constraint_char
-    
-    def join(self, other):
-        assert self.llvm_type == other.llvm_type, "LLVM types do not match."
-        assert self.constraint_char == other.constraint_char, "Constraint chars do not match."
-        if self._name == other._name:
-            # nothing to do, already joined or equal
-            pass
-        else:
-            del self._REGISTER_NAMES_IN_USE[self._name]
-            self._name = other._name
-    
-    def __eq__(self, other):
-        return (self.llvm_type == other.llvm_type and
-                self.constraint_char == other.constraint_char and
-                self._name == other._name)
-    
-    def __hash__(self):
-        return hash((self.llvm_type, self.constraint_char, self._name))
+
 
 class Synthable:
     def __init__(self):
         pass
     
-    def build_ir(self):
+    def build_ir(self, reg_mapping):
         raise NotImplementedError()
     
     def get_source_registers(self):
@@ -187,10 +221,17 @@ class Instruction(Operation):
         else:
             return []
 
-    def build_ir(self):
+    def generate_register_mapping(self, reg_mapping):
+        print(self, 'generate_register_mapping')
+        for r in self.get_source_registers():
+            reg_mapping.sink(r)
+        for r in self.get_destination_registers():
+            reg_mapping.source(r)
+
+    def build_ir(self, reg_mapping):
         '''
         Build IR string based on in and out operand names and types.
-        '''        
+        '''
         # Build constraint string from operands
         constraints = ','.join(
             ['='+self.destination_operand.get_constraint_char()] +
@@ -200,7 +241,9 @@ class Instruction(Operation):
         operands = []
         for sop in self.source_operands:
             if isinstance(sop, Immediate) or isinstance(sop, Register):
-                operands.append('{type} {repr}'.format(type=sop.llvm_type, repr=sop.get_ir_repr()))
+                operands.append('{type} {repr}'.format(
+                    type=sop.llvm_type,
+                    repr=sop.get_ir_repr(reg_mapping)))
             else:
                 raise NotImplemente("Only register and immediate operands are supported.")
         args = ', '.join(operands)
@@ -208,7 +251,7 @@ class Instruction(Operation):
         # Build instruction from instruction and operands
         return ('{dst_reg} = call {dst_type} asm sideeffect'
                 ' "{instruction}", "{constraints}" ({args})').format(
-                    dst_reg=self.destination_operand.get_ir_repr(),
+                    dst_reg=self.destination_operand.get_ir_repr(reg_mapping),
                     dst_type=self.destination_operand.llvm_type,
                     instruction=self.instruction,
                     constraints=constraints,
@@ -233,7 +276,7 @@ class AddressGeneration(Operation):
         self.index = index
         self.width = width
         self.destination = destination
-    # TODO
+        raise NotImplementedError()
 
 
 class Serialized(Synthable):
@@ -258,18 +301,54 @@ class Serialized(Synthable):
             return self.synths[-1].get_destination_registers()
         else:
             return []
+
+    @staticmethod
+    def match(source_registers, destination_registers):
+        '''
+        Find maximum number of matches from source (previous destinations) to
+        destination (current source) registers.
+        
+        Return list of two-tuples of matches (src, dst) and set of unmachted destination registers.
+        '''
+        matched_pairs = []
+        unmatched_dests = set(destination_registers)
+        for dst in destination_registers:
+            for src in source_registers:
+                if src.llvm_type == dst.llvm_type:
+                    matched_pairs.append((src, dst))
+                    unmatched_dests.discard(dst)
+        
+        return matched_pairs, unmatched_dests
     
-    def build_ir(self):
-        code = []
-        last = None
+    def generate_register_mapping(self, reg_mapping=None):
+        if reg_mapping is None:
+            reg_mapping = RegisterMapping()
+
+        # First register all registers for later mapping
         for s in self.synths:
-            last_dests = last.get_source_registers() if last else []
-            matched, unmatched = Register.match(s.get_source_registers(), last_dests)
-            if not matched and last is not None:
+            s.generate_register_mapping()
+
+        last_s = s
+        for s in self.synths:
+            matched_pairs, unmatched_dests = self.match(
+                last_s.get_destination_registers(), s.get_source_registers())
+
+            for p in matched_pairs:
+                #print(reg_mapping.mapping)
+                #print(reg_mapping.source_sink)
+                #print(p)
+                reg_mapping.connect(p)
+            
+            if not matched_pairs:
                 raise ValueError("Could not find a type match to serialize {} to {}.".format(
-                    last, self))
-            code.append(s.build_ir())
-            last = s
+                    last_s, s))
+        return reg_mapping
+
+    def build_ir(self, reg_mapping=None):
+        reg_mapping = self.generate_register_mapping(reg_mapping)
+        code = []
+        for s in self.synths:
+            code.append(s.build_ir(reg_mapping))
         return '\n'.join(code)
 
 
@@ -320,9 +399,9 @@ if __name__ == '__main__':
         destination_operand=Register('i64', 'r'),
         source_operands=[Register('i64', 'r'), Immediate('i64', '23')])
     i6 = Instruction(
-        instruction='add $2, $0',
+        instruction='inc $0',
         destination_operand=Register('i64', 'r'),
-        source_operands=[Register('i64', 'r'), Register('i64', 'r')])
+        source_operands=[Register('i64', 'r')])
     s1 = Serialized([i1, i2])
     s2 = Serialized([s1, i3])
     s2.build_ir()
