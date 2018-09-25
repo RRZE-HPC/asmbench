21fb4d1c4db04e7a8a07ca13922df3ab3fc8b7f1
Julian Hammer <julian.hammer@fau.de>
2018-09-25 10:37:09 +0200

    changed to rst readme

--- README.rst
+++ README.rst
@@ -1,17 +1,17 @@
-# asmbench
+asmbench
+========
 
 A benchmark toolkit for assembly instructions using the LLVM JIT.
 
-## Usage
+Usage
+=====
 
 To benchmark latency and throughput of a 64bit integer add use the following command:
-```
-python -m asmbench 'add {src:i64:r}, {srcdst:i64:r}'
-```
+
+``python -m asmbench 'add {src:i64:r}, {srcdst:i64:r}'``
 
 To benchmark two instructions interleaved use this:
-```
-python -m asmbench 'add {src:i64:r}, {srcdst:i64:r}' 'sub {src:i64:r}, {srcdst:i64:r}'
-```
+
+``python -m asmbench 'add {src:i64:r}, {srcdst:i64:r}' 'sub {src:i64:r}, {srcdst:i64:r}'``
 
 To find out more add `-h` for help and `-v` for verbose mode.
--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+with open('README.rst') as f:
     long_description = f.read()
 
 setup(
     name='asmbench',
-    version='0.1.1',
+    version='0.1.1.2',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    url='',
+    url='https://github.com/RRZE-HPC/asmbench',
     license='AGPLv3',
     author='Julian Hammer',
     author_email='julian.hammer@fau.de',
     description='A Benchmark Toolkit for Assembly Instructions Using the LLVM JIT',
-    long_description_content_type='text/markdown',
     long_description=long_description,
     install_requires=['llvmlite>=0.23.2', 'psutil'],
     extras_require={
